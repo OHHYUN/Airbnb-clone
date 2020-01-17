85524aa880a94da949bba5f60612607e4471f8de
OHHYUN <omplcie@gmail.com>
2020-01-17 20:40:21 +0900

    add Room Admin

--- rooms/models.py
+++ rooms/models.py
@@ -55,7 +55,7 @@ class Room(core_models.TimeStampedModel):
 
     name = models.CharField(max_length=140)
     description = models.TextField()
-    coutry = CountryField()
+    country = CountryField()
     city = models.CharField(max_length=80)
     price = models.IntegerField()
     address = models.CharField(max_length=140)
